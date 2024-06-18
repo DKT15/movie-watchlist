b0953cbb6c1e816206b55536925d66c8b2883f99
Danté Kellman-Thompson <dante.kellman@icloud.com>
2024-06-18 16:01:26 +0100

    added to the JS and CSS.

--- assets/index.js
+++ assets/index.js
@@ -1,21 +1,57 @@
 const formEl = document.getElementById("search");
 const searchEl = document.getElementById("search-movie");
+const filmContainerEl = document.getElementById("film-container");
+
+let moviesArr = [];
+let movieId = [];
 
 formEl.addEventListener("submit", (e) => {
   e.preventDefault();
-  let filmHtml = "";
+  moviesArr = [];
+  movieId = [];
+
   fetch(`http://www.omdbapi.com/?apikey=e56c8dbc&s=${searchEl.value}`)
     .then((res) => res.json())
-    .then(
-      (data) => console.log(data)
-      // data.forEach((film) => {
-      //   filmHtml += `<div class="film-container">
-      //   <img src="${film.poster}>
-
-      //   </div>
-
-      // `;
-      //   console.log(data);
-      // })
-    );
+    .then((data) => {
+      console.log(data);
+      console.log(pushData(data.Search, moviesArr, movieId));
+      renderData(moviesArr);
+    });
 });
+
+function pushData(arr1, arr2, arr3) {
+  for (let movie of arr1) {
+    arr2.push(movie.Title);
+    arr3.push(movie.imdbID);
+  }
+}
+
+function renderData(arr) {
+  filmContainerEl.innerHTML = "";
+  for (let movie of arr) {
+    fetch(`http://www.omdbapi.com/?apikey=e56c8dbc&t=${movie}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        filmContainerEl.innerHTML += `
+        <div class="movie-wrapper">
+          <img class="movie-img" src="${data.Poster}">
+          <div class="text-content">
+              <div class="title"> 
+                  <h3>${data.Title}</h3>
+                  <p class="rating"><i class="fa-solid fa-star"></i> ${data.imdbRating}</p>
+              </div>
+              <div class="line2">
+                  <p>${data.Runtime}</p>
+                  <p>${data.Genre}</p>
+                  <p class="add-to-watchlist" data-watchlist="${data.imdbID}"><i class="fa-solid fa-circle-plus"></i>Watchlist</p>
+              </div>
+              <div class="plot">
+                  <p>${data.Plot}</p>
+              </div>
+          </div>
+        </div>
+        `;
+      });
+  }
+}
