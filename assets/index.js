a2c1425e6db1df45e6c882018bea2a963fb1c706
Danté Kellman-Thompson <dante.kellman@icloud.com>
2024-06-19 20:08:39 +0100

    added to code.

--- assets/index.js
+++ assets/index.js
@@ -2,33 +2,32 @@ const formEl = document.getElementById("search");
 const searchEl = document.getElementById("search-movie");
 const filmContainerEl = document.getElementById("film-container");
 
-let moviesArr = [];
+let moviesTitleArr = [];
 let movieId = [];
 
 formEl.addEventListener("submit", (e) => {
   e.preventDefault();
-  moviesArr = [];
-  movieId = [];
-
   fetch(`http://www.omdbapi.com/?apikey=e56c8dbc&s=${searchEl.value}`)
     .then((res) => res.json())
     .then((data) => {
       console.log(data);
-      console.log(pushData(data.Search, moviesArr, movieId));
-      renderData(moviesArr);
+      pushData(data.Search, moviesTitleArr, movieId); //using the pushData function to get access to each movie that appears in the search. It is then being used to extract each title and imdbID.
+      renderData(moviesTitleArr); //this uses the movie title that has been passed in from the API.
     });
 });
 
 function pushData(arr1, arr2, arr3) {
   for (let movie of arr1) {
-    arr2.push(movie.Title);
-    arr3.push(movie.imdbID);
+    arr2.push(movie.Title); //pushing the moving title onto the moviesTitleArr.
+    arr3.push(movie.imdbID); //pushing the imdbID onto the movieId.
   }
 }
 
-function renderData(arr) {
+//Using the movie title as a result of GET, the api is being used again based on the title of the movie. The HTML code then collects and formats everything that I'm asking from the api which includes, movie title, image, genre etc.
+function renderData(titleArr) {
   filmContainerEl.innerHTML = "";
-  for (let movie of arr) {
+
+  for (let movie of titleArr) {
     fetch(`http://www.omdbapi.com/?apikey=e56c8dbc&t=${movie}`)
       .then((res) => res.json())
       .then((data) => {
